fc58ed2e2eef2f09bfa0a1f7e0b46c5f66dc8911
ZyNTaX <5297699+zyntax9999@users.noreply.github.com>
2026-09-25 15:33:39 +0200

    Update twistr.py

--- twistr.py
+++ twistr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # twistr - typosquatting / phishing lookalike domain scanner
-# Copyright 2026 [YOUR NAME OR GITHUB HANDLE]
+# Copyright 2026 zyntax9999
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
